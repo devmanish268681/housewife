cfe7f0cacf55c724dc54e11500fd81ec2bad85ac
devmanish268681 <manishkumar268681@gmail.com>
2025-06-29 15:09:17 +0530

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import StoreProvider from "@/lib/store/StoreProvider";
+import "./globals.css";
+import { Provider } from "react-redux";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
+    </html>
+  );
+}
