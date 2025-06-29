a117b5571c7c6b6693ef1c6523b0e79ad01f898d
devmanish268681 <manishkumar268681@gmail.com>
2025-06-29 17:25:02 +0530

    desined navbar

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {
-        //reducers
-    },
-  })
+    return configureStore({
+        reducer: {
+            //reducers
+        },
+    })
 }
 
 // Infer the type of makeStore
