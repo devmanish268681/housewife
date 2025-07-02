2562dfb773014dd66cd5e3ab844fe81138388546
devmanish268681 <manishkumar268681@gmail.com>
2025-07-02 22:26:56 +0530

    designed footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import StoreProvider from "@/lib/store/StoreProvider";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import AuthProvider from "@/lib/auth/AuthProvider";
+import Footer from "@/components/footer/Footer";
 
 export default function RootLayout({
   children,
@@ -12,9 +13,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AuthProvider>
-        <Header />
-        {/* <StoreProvider>{children}</StoreProvider> */}
-        {children}
+          <Header />
+          {/* <StoreProvider>{children}</StoreProvider> */}
+          {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
