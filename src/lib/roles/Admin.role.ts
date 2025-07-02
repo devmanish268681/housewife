de10092bea5d924286adba7625a94e222b0d9851
unknown <saurabhkpatil24@gmail.com>
2025-07-02 17:03:35 +0530

    feature-get-all-products-and-by-cateogry

---
+++ src/lib/roles/Admin.role.ts
@@ -0,0 +1,14 @@
+export const admin = {
+  products: {
+    view: true,
+    create: true,
+    update: true,
+    delete: true,
+  },
+  order: {
+    view: true,
+    create: true,
+    update: true,
+    delete: true,
+  },
+};
