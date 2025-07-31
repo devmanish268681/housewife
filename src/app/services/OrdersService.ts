57a59107cab178fee81eed0d44533080e6dab65a
Saurabh_Patil <saurabhkpatil24@gmail.com>
2025-07-31 21:22:16 +0530

    update folder strucutre

--- src/app/services/OrdersService.ts
+++ src/app/services/OrdersService.ts
@@ -1,8 +1,11 @@
-import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
 
-export const createOrderRecord = async (orderObj: any) => {
+export const createOrderRecord = async (
+  tx: Prisma.TransactionClient,
+  orderObj: Prisma.OrderUncheckedCreateInput
+) => {
   try {
-    const order = await prisma.order.create({ data: orderObj });
+    const order = await tx.order.create({ data: orderObj });
     return order;
   } catch (error: any) {
     console.error("Internal server error", error);
