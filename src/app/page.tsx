f6ca8ce744f687ab3dbbc7d905c8a3dc12885747
Saurabh_Patil <saurabhkpatil24@gmail.com>
2025-08-29 15:49:48 +0530

    bug fix webscoket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import HomePage from "@/components/home/Home";
 
 //utils
 import ClientGeoLocation from "./ClientGeoLocation";
+import { pingExternalServerOnce } from "../lib/ping-once";
 
 export default async function Home() {
-
+  await pingExternalServerOnce();
   return (
     <>
       <HeroBanner />
